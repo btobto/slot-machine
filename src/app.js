00eb4e9dec94aea79b682bc115be10b1558e8de0
btobto <btosic16@gmail.com>
2022-04-09 18:05:53 +0200

    start

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import * as PIXI from 'pixi.js';
+import { Player } from './player';
 
 const app = new PIXI.Application(640, 360, {
 
@@ -6,25 +7,16 @@ const app = new PIXI.Application(640, 360, {
 
 document.body.appendChild(app.view);
 
-const weights = [
-    10,
-    9,
-    9,
-    7,
-    8,
-    
-];
+const player = new Player(500, 100);
 
-const special = {};
-
-let mat = [[]];
-
-for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-        mat[i][j] = (Math.random * 10) % 8;
-
-    }
-
-
-
-}
+PIXI.loader
+    .add("s1", "./assets/images/s1.png")
+    .add("s2", "./assets/images/s2.png")
+    .add("s3", "./assets/images/s3.png")
+    .add("s4", "./assets/images/s4.png")
+    .add("s5", "./assets/images/s5.png")
+    .add("s6", "./assets/images/s6.png")
+    .add("s7", "./assets/images/s7.png")
+    .add("s8", "./assets/images/s8.png")
+    .add("coin", "./assets/images/coin.png")
+    .add("spin", "./assets/images/spin.png")
---
+++ src/player.js
@@ -0,0 +1,20 @@
+export class Player {
+	constructor(balance, stake) {
+		this.balance = balance;
+		this.stake = stake;
+	}
+
+	reduceBalance() {
+		this.balance -= this.stake;
+
+		if (this.balance <= 0) {
+			// todo
+		}
+	}
+
+	changeStake(newStake) {
+		if (newStake <= this.stake) {
+			this.stake = newStake;
+		}
+	}
+}

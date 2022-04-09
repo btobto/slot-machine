d195dc80f897d99bc0f11ecf3d6811dbf8e6a3f6
andrija-tosic <andonnet@gmail.com>
2022-04-10 01:37:58 +0200

    calculation api for client

--- src/calculation.js
+++ src/calculation.js
@@ -1,46 +1,25 @@
+import { Player } from './player.js';
+
 const combos =
 {
-    '111': 10,
-    '222': 6,
-    '333': 6,
-    '444': 5,
-    '555': 5,
-    '224': 4,
-    '334': 3,
-    '666': 3,
-    '332': 2,
-    '442': 2,
-    '552': 2,
-    '662': 2,
-    '77*': 1
-};
-
-const counts =
-{
-    '111': 0,
-    '222': 0,
-    '333': 0,
-    '444': 0,
-    '555': 0,
-    '224': 0,
-    '334': 0,
-    '666': 0,
-    '332': 0,
-    '442': 0,
-    '552': 0,
-    '662': 0,
-    '77*': 0
+    '111': 120,
+    '222': 65,
+    '333': 50,
+    '444': 25,
+    '555': 20,
+    '224': 15,
+    '334': 10,
+    '666': 8,
+    '332': 7,
+    '442': 6,
+    '552': 5,
+    '662': 4,
+    '77*': 2
 };
 
 const log = false;
 
-const oldreel1 = [2, 2, 7, 3, 2, 4, 5, 5, 5, 3, 5, 3, 5, 7, 7, 2, 2, 6, 5, 3, 1, 5, 7, 3, 6];
-const oldreel2 = [4, 5, 6, 2, 3, 1, 8, 6, 3, 2, 4, 2, 8, 7, 6, 4, 8, 6, 5, 2, 4, 4, 5, 6, 3, 2];
-const oldreel3 = [1, 3, 2, 6, 2, 4, 3, 6, 8, 4, 2, 4, 6, 4, 5, 4, 6, 4, 4, 5, 8, 3, 5, 6, 4, 3, 6, 8, 6, 4, 2, 8, 4, 5, 6];
-
-
 let reel1, reel2, reel3;
-
 
 reel1 = [1, 2, 4, 5, 2, 2, 4, 7, 3, 6];
 reel2 = [6, 2, 6, 5, 7, 3, 1, 7, 3, 3, 4, 5];
@@ -52,19 +31,26 @@ reel3 = [6, 1, 2, 6, 3, 7, 5, 2, 5, 3, 4, 4, 6, 2];
 
 const reels = [reel1, reel2, reel3];
 
-let mat = [];
-
-const startingBalance = 10000000;
-
-let balance = startingBalance;
-
-const stake = 15;
-
-function randomizeMat(mat, stake) {
+function spinSlot(player) {
+    if (player.balance < player.stake) {
+        console.log("No money left!");
+        return;
+    }
+    
+    let mat = [];
     for (let i = 0; i < 3; i++) {
         mat[i] = [];
     }
 
+    randomizeMat(mat, player);
+
+    let returnVal = check(mat);
+
+    player.balance -= stake;
+    player.balance += returnVal.points;
+}
+
+function randomizeMat(mat, player) {
     let i = 0;
     reels.forEach(reel => {
         let randomIndex = parseInt(Math.random() * 100, 10);
@@ -111,7 +97,6 @@ function check(mat) {
         }
 
     }
-
 
     // upper zig-zag
     if (mat[0][0] === 7 && mat[1][1] === 7 && mat[0][2] !== 7) {
@@ -160,36 +145,5 @@ function check(mat) {
 
     }
 
-
     return { lineHits, points };
 }
-
-const iterations = 1000000;
-
-let lineHits = 0;
-let i = 0;
-
-for (i = 0; i < iterations; i++) {
-
-    if (balance < stake) {
-        console.log("Money is at 0");
-        break;
-    }
-
-    randomizeMat(mat);
-
-    let returnVal = check(mat);
-
-    lineHits += returnVal.lineHits;
-
-    balance += returnVal.points;
-    balance -= stake;
-}
-
-console.log(`Iterations: ${i}`);
-
-console.log(`Ratio of lineHits : ${(lineHits / iterations) * 100}%`);
-console.log(`balance ${balance}, startingBalance ${startingBalance}`);
-console.log(`RTP: ${(balance / startingBalance) * 100}%`);
-
-console.log(counts);

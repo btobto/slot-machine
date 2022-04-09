a57f6505b06af58ea87301661281352f078aadfe
andrija-tosic <andonnet@gmail.com>
2022-04-09 18:33:24 +0200

    pocetna implementacija i testiranje

--- src/app.js
+++ src/app.js
@@ -6,25 +6,3 @@ const app = new PIXI.Application(640, 360, {
 
 document.body.appendChild(app.view);
 
-const weights = [
-    10,
-    9,
-    9,
-    7,
-    8,
-    
-];
-
-const special = {};
-
-let mat = [[]];
-
-for (let i = 0; i < 3; i++) {
-    for (let j = 0; j < 3; j++) {
-        mat[i][j] = (Math.random * 10) % 8;
-
-    }
-
-
-
-}
